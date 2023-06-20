0a32abbc11a18bf7439d3ebf146c1d6d0b8b7096
logmar98 <marwanarbib15@gmail.com>
2023-06-20 14:01:00 -0700

    add a file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,12 @@
 #ifndef main_h
 #define main_h
 
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isappha(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
@@ -13,6 +14,8 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void rint_times_table(int n);
+void print_times_table(int n);
+
 
 #endif
+
