24fec69eb04f015a446536a70d0023958620d67e
logmar98 <marwanarbib15@gmail.com>
2023-09-14 11:45:52 -0700

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+int n = 0;
+while (h != NULL)
+{
+n++;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (n);
+}
+
