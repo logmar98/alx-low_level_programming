978bf89cfb3ac8ad040ba9d302b83de301a66fdc
logmar98 <marwanarbib15@gmail.com>
2023-09-14 14:26:25 -0700

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_dnodeint - is add a new node in begging
+ *
+ * @head: is node
+ * @n: is the data of the node
+ * Return: new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+struct dlistint_s *new_node;
+new_node = malloc(sizeof(struct dlistint_s));
+new_node->n = n;
+
+new_node->next = (*head);
+new_node->prev = NULL;
+
+if ((*head) != NULL)
+{
+(*head)->prev = new_node;
+}
+(*head) = new_node;
+return (*head);
+}
+
