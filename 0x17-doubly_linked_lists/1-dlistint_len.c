f631f0d9b1f362cac11d400a342b01fc834c88bc
logmar98 <marwanarbib15@gmail.com>
2023-09-14 11:48:23 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * print_dlistint - prints all the elements of a
+ * dlistint_len - prints the lenght of a
  * dlistint_t list
  *
  * @h: head of the list
- * Return: the number of nodes
+ * Return: the len of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
