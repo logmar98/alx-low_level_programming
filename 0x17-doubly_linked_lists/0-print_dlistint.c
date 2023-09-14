16b25ea46b3056ef4b67f927de91880292302a01
logmar98 <marwanarbib15@gmail.com>
2023-09-14 11:20:25 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include "list.h"
+size_t print_dlistint(const dlistint_t *h)
+{
+    int n = 0;
+
+    while (h != NULL) {
+        n++;
+        printf("%i\n", h->n);
+        h = h->next;
+    }
+    return n;
+}
