66d47d52ac7ec770bdf260cc801d8b0beea61b0d
logmar98 <marwanarbib15@gmail.com>
2024-02-06 15:29:38 +0100

    update task0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,13 +10,13 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	int i = 0;
 
 	if (array == NULL)
 		return (-1);
-	while (i < size)
+	while (i < (int)size)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
@@ -24,5 +24,4 @@ int linear_search(int *array, size_t size, int value)
 		i++;
 	}
 	return (-1);
-
 }
