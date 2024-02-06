108c1da81dcb2b70ad65990213bf852aed874502
logmar98 <marwanarbib15@gmail.com>
2024-02-06 15:38:38 +0100

    update header

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ * linear_search - use linear search to search
+ *
+ *  @array: array list of numbers.
+ *  @size: size of the array.
+ *  @value: the value that need to find
+ * Return: the index of the value if it find - otherwise -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+    
+}
